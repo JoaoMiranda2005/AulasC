ce13a54e2670019c19f9ab15d19695c6bcdce359
João Miranda <jprmiranda678@gmail.com>
2023-11-15 18:01:08 +0000

    ex 1 added

---
+++ Aula 15-11/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float temperaturaFahrenheit, temperaturaCelsius;
+    printf("Digite a temperatura em Fahrenheit: ");
+    scanf("%f", &temperaturaFahrenheit);
+    temperaturaCelsius = (5 * (temperaturaFahrenheit - 32)) / 9;
+    printf("Temperatura em Fahrenheit: %.2f\n", temperaturaFahrenheit);
+    printf("Temperatura em Celsius: %.2f\n", temperaturaCelsius);
+
+    return 0;
+}
